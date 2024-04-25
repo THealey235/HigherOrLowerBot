9d12315e2f31835c8c77d6afb4e46b0cc15ec70f
Biscoui <tom.healey.235@gmail.com>
2024-04-25 22:17:36 +0100

    Screenshot procedure
    Added Screenshot procedure which takes a bitmamp image on the primary monitor

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing.Imaging;
+using System.Drawing;
+
+static class Program
+{
+    static string mode;
+    static Rectangle rectLeft = new Rectangle(123, 366, 772, 290);
+    static Rectangle rectRight = new Rectangle(1028, 365, 870, 300);
+
+    public static void Main()
+    {
+        //Console.Write("Mode (learn/do): ");
+        mode = Console.ReadLine();
+        Screenshot(rectLeft, 0);
+        Screenshot(rectRight, 1);
+
+    }
+
+    //takes screenshot on primary monitor on co-ordinates specified in Rectangle parameter, Saves it in same file 
+    static void Screenshot(Rectangle captureRectangle, int rectNum)
+    {
+        try
+        {
+            Bitmap captureBitmap = new Bitmap(1024, 768, PixelFormat.Format32bppArgb);
+            Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+            captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+            captureBitmap.Save(@$"C:\dev\HigherOrLowerBot\Capture{rectNum}.jpg", ImageFormat.Jpeg);
+            Console.WriteLine("Capture Taken!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Capture Failed");
+        }
+    }
+}
+
